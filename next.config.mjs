6a79ad135a13f0613566ed5719c76379c515f503
unknown <owilneto@gmail.com>
2024-08-19 15:26:11 -0300

    Atualiza configuração do Next.js para suportar imagens externas

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['icflow.vercel.app'], // Adicione outros domínios se necessário
+    },
+    // Outras configurações podem ser adicionadas aqui
+  };
+  
+  export default nextConfig;
+  
