dd8c905bb3917cba23922c387c04fdd5f27dfad4
unknown <owilneto@gmail.com>
2024-09-02 12:18:19 -0300

    Atualizando middleware para imagens

--- middleware.ts
+++ middleware.ts
@@ -6,8 +6,13 @@ import { NextRequest } from 'next/server';
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
 
-  // Se não houver token e não estiver tentando acessar a página de login ou registro
-  if (!token && !['/login', '/register'].includes(req.nextUrl.pathname)) {
+  // Se não houver token e não estiver tentando acessar a página de login, registro ou recursos estáticos
+  if (!token && !['/login', '/register'].includes(req.nextUrl.pathname) &&
+      !req.nextUrl.pathname.startsWith('/_next/') && // Ignora caminhos internos do Next.js
+      !req.nextUrl.pathname.startsWith('/static/') && // Ignora recursos estáticos personalizados
+      !req.nextUrl.pathname.startsWith('/images/') && // Ignora imagens
+      !req.nextUrl.pathname.startsWith('/favicon.ico') // Ignora o favicon
+  ) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
